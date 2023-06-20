c5da8e58f549fe3760addf47cf36c4f9b018605d
Streatman <TheStreatman@gmail.com>
2023-06-20 22:08:52 +0200

    Discord Link

--- src/scripts/4_World/Static/WatermarkHandler.c
+++ src/scripts/4_World/Static/WatermarkHandler.c
@@ -2,6 +2,7 @@ class WatermarkHandler : Managed
 {
 	void OnWatermarkClick()
 	{
+		// Replace it with your own Discord link if you want, or delete the lines in the other Files to remove the link completly
 		GetGame().OpenURL("https:/" + "/dis" + "cord" + ".gg" + "/Ec5t3" + "MwnaE");
 	}
 };
