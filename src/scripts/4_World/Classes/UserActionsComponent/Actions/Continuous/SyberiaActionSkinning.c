bdfb79442756ff45fc65f10b7c25c9f096f848f0
Streatman <TheStreatman@gmail.com>
2023-08-24 16:51:26 +0200

    Vanilla Skinning

--- src/scripts/4_World/Classes/UserActionsComponent/Actions/Continuous/SyberiaActionSkinning.c
+++ src/scripts/4_World/Classes/UserActionsComponent/Actions/Continuous/SyberiaActionSkinning.c
@@ -2,44 +2,24 @@ modded class ActionSkinningCB
 {
 	override void CreateActionComponent()
 	{
-		float skinTime = 30;		
-		if (m_ActionData.m_Player) {
-			float modifier = 1 - m_ActionData.m_Player.GetPerkFloatValue(SyberiaPerkType.SYBPERK_HUNTING_BUTCH_SPEED, 0, 0);
-			skinTime = Math.Max(1, skinTime * modifier);
+		float skinTime = 30;
+		float TimeModifier = skinTimeModifier();
+		if (m_ActionData.m_Player)
+		{
+			float skillModifier = 1 - m_ActionData.m_Player.GetPerkFloatValue(SyberiaPerkType.SYBPERK_HUNTING_BUTCH_SPEED, 0, 0);
+			skinTime = Math.Max(1, skinTime * skillModifier * TimeModifier);
 		}
 		
 		m_ActionData.m_ActionComponent = new CAContinuousTime( skinTime );
 	}
-};
-
-modded class ActionSkinning
-{
-	override string GetText()
+	float skinTimeModifier()
 	{
-		return "#syb_skinning_action";
-	}
-	
-	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
-	{
-		Object targetObject = target.GetObject();
-		if ( targetObject != NULL )
+		Edible_Base m_Target = Edible_Base.Cast(m_ActionData.m_Target.GetObject());
+		if (m_Target.ConfigIsExisting("skinTimeModifier"))
 		{
-			if ( targetObject.CanBeSkinned() )
-			{
-				AnimalBase animal = AnimalBase.Cast(targetObject);
-				if (animal && !animal.IsSkinned() && !animal.IsAlive())
-				{
-					return true;
-				}
-				
-				Edible_Base edibleBase = Edible_Base.Cast(targetObject);
-				if (edibleBase && !edibleBase.IsSkinned())
-				{
-					return true;
-				}
-			}
+			float skinTimeModifier = m_Target.ConfigGetFloat("skinTimeModifier");
+			return skinTimeModifier;
 		}
-		
-		return false;
-	}
+		return 1;
+	};
 };
